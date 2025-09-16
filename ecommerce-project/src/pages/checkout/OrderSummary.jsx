879b76557f8401c9509fef582a63bd2dbf5340cc
martin <martingonzalezmar@gmail.com>
2025-09-16 21:59:37 +0200

    separate into smaller components

---
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs'
+import { CartItemDetails } from './CartItemDetails';
+import { DeliveryOptions } from './DeliveryOptions';
+
+export function OrderSummary({ cart, deliveryOptions }) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+        const selectedDeliveryOption = deliveryOptions
+          .find((deliveryOption) => {
+            return deliveryOption.id === cartItem.deliveryOptionId;
+          });
+
+        return (
+          <div key={cartItem.productId} className="cart-item-container">
+            <div className="delivery-date">
+              Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+            </div>
+
+            <div className="cart-item-details-grid">
+              <img className="product-image"
+                src={cartItem.product.image} />
+              <CartItemDetails cartItem={cartItem} />
+              <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
